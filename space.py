f625b2f9a5c9d34d1257ceecd023bf6963710615
Ryan Nguyen <ryan.q.nguyen@gmail.com>
2020-09-20 18:58:10 -0600

    Add files via upload

---
+++ space.py
@@ -0,0 +1,52 @@
+import turtle
+import os
+
+#setup screen
+win = turtle.Screen()
+win.title("Space Invaders")
+win.bgcolor("black")
+
+#create a boarder
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+
+#create the player 
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+playerspeed = 15
+
+#Move the player 
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if x < -280:
+        x = -280
+    player.setx(x)
+
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if x > 280:
+        x = 280
+    player.setx(x)
+#Create keyboard bindings
+turtle.listen()
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_right, "Right")
+
